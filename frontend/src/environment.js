d9d1db3765c4e405c1d7f6993063d0e76c359785
Mayank <mayankjha8274@gmail.com>
2026-01-11 23:08:22 +0530

    Deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,10 +1,6 @@
-// let IS_PROD = true;
-// const server = IS_PROD ?
-//     "https://apnacollegebackend.onrender.com" :
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://meettrack-ai.onrender.com" :
 
-//     "http://localhost:8000"
+    "http://localhost:8000"
 
-const server = "http://localhost:8000";
-
-
-export default server;
